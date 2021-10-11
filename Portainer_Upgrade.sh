6d2eaa8f0ffc9864e4ed56cadd6bee0a7a41e458
Xavier Wang <XavierWang3P@users.noreply.github.com>
2021-10-11 15:19:28 +0800

    Create Portainer_Upgrade.sh

---
+++ Portainer_Upgrade.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#Remove the old Portainer container
+docker stop portainer
+docker rm portainer
+
+#Pull the latest Portainer image
+docker pull portainer/portainer-ce:latest
+
+#Deploy Portainer container
+docker run -d -p 8000:8000 -p 9000:9000 -p 9443:9443 \
+    --name=portainer --restart=always \
+    -v /var/run/docker.sock:/var/run/docker.sock \
+    -v portainer_data:/data \
+    portainer/portainer-ce:latest
