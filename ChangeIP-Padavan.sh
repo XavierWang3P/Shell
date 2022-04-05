067a7229e4690e66dbd354b5eae2eb712162be6a
Xavier Wang <XavierWang3P@users.noreply.github.com>
2022-04-05 20:40:18 +0800

    扩充 IP 池、网络状态使用 HTTP 状态码检测、调整 pushplus 推送信息

--- ChangeIP-Padavan.sh
+++ ChangeIP-Padavan.sh
@@ -1,37 +1,51 @@
 #!/bin/sh
 
-ip_1="192.168.154.190"
-ip_2="192.168.154.189"
+ip_1="192.168.154.186"
+ip_2="192.168.154.187"
+ip_3="192.168.154.190"
+ip_4="192.168.154.191"
 current_wan_ipaddr=$(nvram get wan_ipaddr)
-surf_internet=$(curl http://detectportal.firefox.com/success.txt)
-judge_success="success"
+surf_internet_firefox=$(curl -i -s http://detectportal.firefox.com/success.txt | grep 'HTTP' | tr -d "A-Z1./" | tr -d " ")
 
-# 判断是否在线
+# Online or not
 if [ "$surf_internet" = "$judge_success" ]; then
-    logger -t "【当前状态】" "在线"
+    logger -t "[Status]" "Online"
 else
-    logger -t "【当前状态】" "已掉线"
+    logger -t "[Status]" "Offline"
 
-    # 获取当前 WAN IP
-    if [ "$current_wan_ipaddr" = "$ip_1" ]; then
+    # Get current WAN IP
+    if [ "$current_wan_ipaddr" = "$ip_1"  ]; then
+        logger -t "[Change IP]" "$ip_2"
         nvram set wan_ipaddr=$ip_2
         restart_wan
-        logger -t "【更改 IP】" "$ip_2"
+    elif [ "$current_wan_ipaddr" = "$ip_2"  ]; then
+        logger -t "[Change IP]" "$ip_3"
+        nvram set wan_ipaddr=$ip_3
+        restart_wan
+    elif [ "$current_wan_ipaddr" = "$ip_3"  ]; then
+        logger -t "[Change IP]" "$ip_4"
+        nvram set wan_ipaddr=$ip_4
+        restart_wan
     else
+        logger -t "[Change IP]" "$ip_1"
         nvram set wan_ipaddr=$ip_1
         restart_wan
-        logger -t "【更改 IP】" "$ip_1"
     fi
     sleep 3
 
-    # 登录校园网
+    # Login Internet
     curl -d '{Dr.com 登录参数}' http://dr.com/a30.html
     sleep 3
     
-    # 判断是否登录成功
-    if [ "$surf_internet" = "$judge_success" ]; then
-        logger -t "【登录校园网】" "成功"
+    # Send the notice
+    notice_text='{"token":"pushplus Token","title":"断网啦断网啦","content":"'"${current_wan_ipaddr}"'","template":"json"}'
+    curl -s -o /dev/null -H "Content-Type: application/json" -X POST -d "${notice_text}" https://www.pushplus.plus/send
+    
+    # Online or not?
+    surf_internet_microsoft=$(curl -i -s  http://www.msftconnecttest.com/connecttest.txt | grep 'HTTP')
+    if [ $surf_internet_microsoft -eq 200 ]; then
+        echo "[Login] Success"
     else
-        logger -t "【登录校园网】" "失败"
+        echo "[Login] Failed"
     fi
 fi
